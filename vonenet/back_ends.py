a81b26b5bdeec9b11075dd75b0eeb4880d05ca95
comeeasy <zz1236zz@naver.com>
2021-03-29 19:29:16 +0900

    update

--- vonenet/back_ends.py
+++ vonenet/back_ends.py
@@ -371,9 +371,9 @@ class ConvNet(nn.Module):
 
         # For the sake of studying ML, I will use deeper network
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3, 3), stride=(1, 1), padding=1)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(5, 5), stride=(2, 2), padding=2)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), stride=(1, 1), padding=1)
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=(1, 1), padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=5, stride=5)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.fc1 = nn.Sequential(
             nn.Linear(128, 128),
             # nn.ReLU(inplace=8
@@ -397,14 +397,12 @@ class ConvNet(nn.Module):
         # Batch Normalization(x)
         x = self.pool(self.relu(self.conv2(x)))
         x = self.conv3(x)
-        # Batch Normalization(x)
-
 
         # Flattening
         before_fc = x.size(0)
         x = x.view(x.size(0), -1)
 
-        # print(x.shape)
+        print(x.shape)
 
         x = self.fc1(x)
         x = self.fc2(x)
