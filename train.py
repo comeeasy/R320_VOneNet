ec910e22d2ad2046dca79b72c3e7dbc38fc46642
comeeasy <zz1236zz@naver.com>
2021-11-04 00:43:06 +0900

    feature: case dividing - mnist, imagenet

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 import torch.utils.tensorboard as tensorboard
+from torchvision import datasets
 
 import vonenet.vonenet as vonenet
 
@@ -13,7 +14,7 @@ import argparse
 
 
 
-def vonenet_model_train(epochs, batch_size, lr, image_size, model_arch):
+def vonenet_model_train(epochs, batch_size, lr, image_size, model_arch, dataset):
     if not model_arch in ["resnet18", "resnet50"] :
         logging.error(f"model_arch: {model_arch}")
         raise ValueError()
@@ -25,10 +26,22 @@ def vonenet_model_train(epochs, batch_size, lr, image_size, model_arch):
 
     # get data
     logging.info("loading data")
-    train_data, label_data = data.get_mnist(batch_size, image_size=image_size)
+    if dataset.lower() == 'mnist':
+        train_data, label_data = data.get_mnist(batch_size, image_size=image_size)
+        in_channel = 1
+    elif dataset.lower() == 'imagenet':
+        train_data, label_data = data.get_imagenet(
+            root='/media/r320/2d365830-836f-4d91-8998-fef7c8443335/ImageNet_dset/ILSVRC2012',
+            img_size=image_size,
+            batch_size=batch_size,
+            num_worker=8
+        )
+        in_channel = 3
+    else:
+        raise RuntimeError("Not Exist dataset")
 
     logging.info(f"load vonenet-{model_arch}")
-    model = vonenet.VOneNet(model_arch=model_arch)
+    model = vonenet.VOneNet(model_arch=model_arch, in_channel=in_channel)
     model.eval().to(device)
 
     optimizer = optim.Adam(params=model.parameters(), lr=lr)
@@ -62,7 +75,6 @@ def vonenet_model_train(epochs, batch_size, lr, image_size, model_arch):
 
 
 
-
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
@@ -74,9 +86,11 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=8)
     parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--model_arch', type=str, required=True, default="resnet18",
-                        help='arch: ["resnet18", "resnet18_28x28] default="resnet18"')
+                        help='arch: ["resnet18", "resnet18_28x28"] default="resnet18"')
     parser.add_argument('--img_size', type=int, required=True, default=224,
                         help='shape=(img_size, img_size')
+    parser.add_argument('--dataset', type=str, required=True, default='imagenet',
+                        help='choose one of [imagenet , mnist]')
 
     FLAGS, FIRE_FLAGS = parser.parse_known_args()
 
@@ -85,13 +99,15 @@ if __name__ == '__main__':
     model_arch = FLAGS.model_arch
     learning_rate = float(FLAGS.lr)
     image_size = (int(FLAGS.img_size), int(FLAGS.img_size))
+    dataset = FLAGS.dataset
 
     logging.info(f"As resnet was trained with ImageNet dataset, image size is fixed as (224, 224)")
     logging.info(f"epochs       : {epochs}")
     logging.info(f"batch size   : {batch_size}")
     logging.info(f"model_arch   : {model_arch}")
     logging.info(f"learning_rate: {learning_rate}")
-    logging.info(f"image size    : {image_size}")
+    logging.info(f"image size   : {image_size}")
+    logging.info(f"dataset      : {dataset}")
 
     vonenet_model_train(epochs=epochs, batch_size=batch_size, lr=learning_rate,
-                        image_size=image_size, model_arch=model_arch)
+                        image_size=image_size, model_arch=model_arch, dataset=dataset)
