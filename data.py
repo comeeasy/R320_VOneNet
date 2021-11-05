1474287b1e9d6502f5d12827fc2b4d5c8fcd5966
comeeasy <zz1236zz@naver.com>
2021-11-05 23:43:45 +0900

    Add: get_imagenet, get_damagenet

--- data.py
+++ data.py
@@ -35,6 +35,101 @@ def get_mnist(batch_size=64, image_size=224) :
 
     return mnist_train_dataloader, mnist_val_dataloader
 
+def get_imagenet(root: str, img_size: tuple, batch_size: int, num_worker=4):
+    """
+        parameters: root: str, img_size: int, batch_size: int
+        returns: (train_dataset, val_dataset)
+
+        root:
+            dataset file structure must be same as below
+        ===================
+            root/
+                |-train
+                |-val
+        ===================
+
+        img_size:
+            Resize images as of img_size
+
+        batch_size:
+            batch_size
+    """
+
+    train_path = os.path.join(root, 'train')
+    val_path = os.path.join(root, 'val')
+
+    imagenet_train = ImageFolder(
+        root=train_path,
+        transform=transforms.Compose([
+            transforms.Resize(img_size),
+            transforms.ToTensor()
+        ])
+    )
+
+    imagenet_val = ImageFolder(
+        root=val_path,
+        transform=transforms.Compose([
+            transforms.Resize(img_size),
+            transforms.ToTensor()
+        ])
+    )
+
+    train_dataloader = DataLoader(
+        batch_size=batch_size,
+        dataset=imagenet_train,
+        shuffle=True,
+        num_workers=num_worker,
+        drop_last=True,
+    )
+
+    val_dataloader = DataLoader(
+        batch_size=batch_size,
+        dataset=imagenet_val,
+        shuffle=True,
+        num_workers=num_worker,
+        drop_last=True,
+    )
+
+    return (train_dataloader, val_dataloader)
+
+def get_damegenet(root: str, img_size: tuple, batch_size: int, num_worker=4):
+    """
+        parameters: root: str, img_size: int, batch_size: int
+        returns: adversarial_val_dataset
+
+        root:
+            dataset file structure must be same as below
+        ===================
+            root/
+                |-DAmagenet
+        ===================
+
+        img_size:
+            Resize images as of (img_size, img_size)
+
+        batch_size:
+            batch_size
+    """
+    val_path = os.path.join(root, 'DAmageNet')
+
+    imagenet_val = ImageFolder(
+        root=val_path,
+        transform=transforms.Compose([
+            transforms.Resize(img_size),
+            transforms.ToTensor()
+        ])
+    )
+
+    val_dataloader = DataLoader(
+        batch_size=batch_size,
+        dataset=imagenet_val,
+        shuffle=True,
+        num_workers=num_worker,
+        drop_last=True,
+    )
+
+    return val_dataloader
+
 if __name__ == "__main__" :
     get_mnist()
 
