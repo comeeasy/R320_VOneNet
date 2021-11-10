eef287b4fa15c82594ce333428e57fff2b6a6fde
comeeasy <zz1236zz@naver.com>
2021-11-11 00:37:15 +0900

    rm: useless code

--- val.py
+++ val.py
@@ -18,43 +18,6 @@ import config
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-# def calc_accuracy(batch_size, model_path, image_size, dataset, 
-#                   dset_root, val_method, damagenet_root):
-    
-#     model_path = os.path.abspath(model_path)
-#     if not os.path.isfile(model_path):
-#         raise FileNotFoundError(f"{model_path} is not exist")
-
-#     logging.info(f"Getting initial Accuracy of {model_path} trained by {dataset}")
-#     logging.info(f"accuracy of original data, adversarial data will be written at tensorboard. please check latest data if runs directory.")
-#     logging.info(f"Usage: $ tensorboard --logdir ./runs --bind_all")
-
-#     logging.info(f"loading dataset {dataset}")
-#     if dataset.lower() == 'mnist':
-#         train_dset, val_dset = data.get_mnist(batch_size=batch_size, image_size=image_size)
-#     elif dataset.lower() == 'imagenet':
-#         train_dset, val_dset = data.get_imagenet(root=dset_root, img_size=image_size, batch_size=batch_size)
-
-#     if val_method.lower() == 'damagenet':
-#         if dataset.lower() != 'imagenet':
-#             raise RuntimeError("damagenet validation method only supports imagenet")
-
-#     device = "cuda" if torch.cuda.is_available() else "cpu"
-#     logging.info(f"Use {device}")
-
-#     logging.info(f"loading model {model_path}")
-#     model = torch.load(model_path)
-#     model.eval().to(device)
-    
-#     if val_method.lower() == 'fgsm':
-#         Fgsm.calc_accuracy(model, val_dset)
-
-#     elif val_method.lower() == 'damagenet':
-#         if dataset.lower() != 'imagenet':
-#             raise RuntimeError("To validate with DAmageNet, must choose imagenet as dataset")
-
-#         DamageNet.calc_accuracy(model, damagenet_root, dset_root, image_size, batch_size)
-
 
 def validation(epochs, batch_size, learning_rate, model_path, model_arch, 
                 image_size, dataset, dset_root, val_method,
